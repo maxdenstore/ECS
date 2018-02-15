1e183dc7a7ffcef30cf006276469def09c5ba987
nicho1991 <ladefoged1991@gmail.com>
2018-02-15 11:45:02 +0100

    tilføjet unit test lib

---
+++ ECS.Legacy.Test.Unit/HeaterUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy.Test.Unit
+{
+    [TestFixture]
+    public class HeaterUnitTest
+    {
+        [Test]
+        public void runSelfTest_returnsTrue()
+        {
+            var uut = new Heater();
+            Assert.That(uut.RunSelfTest(), Is.True);
+        }
+
+        [Test]
+        public void runSelfTest_returnsTrue()
+        {
+            var uut = new Heater();
+            Assert.That(uut.RunSelfTest(), Is.True);
+        }
+
+    }
+}
