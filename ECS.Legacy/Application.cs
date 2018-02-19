da87a8ea288731b0bacd45f08d869224ddbbbac9
Max <maxbj@live.dk>
2018-02-19 17:54:47 +0100

    Startet på: Extension 2: Make the thresholds configurable so that they may be changed at run-time.

--- ECS.Legacy/Application.cs
+++ ECS.Legacy/Application.cs
@@ -1,16 +1,23 @@
-﻿namespace ECS.Legacy
+﻿using System;
+
+namespace ECS.Legacy
 {
     public class Application
     {
         public static void Main(string[] args)
         {
-            var ecs = new ECS(28,new TempSensor(), new Heater());
+            //rand 20-29 sensor set.
+            var ext = new ECSExtention(); 
+            ext.setRandom();
+            var ecs = new ECS(ext.thresholdsChange,new TempSensor(), new Heater());
 
             ecs.Regulate();
 
             ecs.SetThreshold(20);
 
             ecs.Regulate();
+
+            Console.WriteLine(ext.thresholdsChange);
         }
     }
 }
---
+++ ECS.Legacy/ECSExtention.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+
+namespace ECS.Legacy
+{
+    public class ECSExtention
+    {
+        public int thresholdsChange { get; set; }
+
+        public void setRandom()
+        {
+            Random rnd = new Random();
+            thresholdsChange = rnd.Next(20, 30);
+           
+        }
+    }
+        
+  
+}
